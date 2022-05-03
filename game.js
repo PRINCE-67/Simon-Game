7cc33485a1d0bfec7d097d533d8d63c51f1b5c22
Prince Verma <princeverma694269@gmail.com>
2022-05-03 13:55:27 +0530

    Simon game project

---
+++ game.js
@@ -0,0 +1,86 @@
+var buttonColours = [];
+var gamePattern = [];
+var userClickedPattern=[];
+
+
+buttonColours.push("red", "blue", "green", "yellow");
+
+$(".btn").click(function (){
+
+var userChosenColour=$(this).attr("id");
+userClickedPattern.push(userChosenColour);
+
+playSound(userChosenColour);
+animatePress(userChosenColour);
+checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel){
+  if(userClickedPattern[currentLevel] === gamePattern[currentLevel])
+  {
+    console.log("success");
+
+    if(userClickedPattern.length === gamePattern.length)
+    {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  }
+  else{
+    $("#level-title").text("Game over, Press any key to restart");
+    var wrongSound=new Audio("sounds/wrong.mp3");
+    wrongSound.play();
+
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+     startOver();
+  }
+}
+
+var level=0;
+var started=false;
+
+$(document).keydown(function (){
+   if(!started){
+      $("#level-title").text("Level "+ level);
+      nextSequence();
+      started=true;
+   }
+});
+
+function nextSequence() {
+  userClickedPattern=[];
+
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor((Math.random())* 4);
+  randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name){
+  var audio= new Audio("sounds/"+ name +".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("#"+ currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#"+ currentColour).removeClass("pressed");
+  },100);
+}
+
+function startOver(){
+  level=0;
+  started=false;
+  gamePattern = [];
+}
